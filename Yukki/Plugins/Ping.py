b7fa991138b9a242c3b79a1cb8344317e35d56d9
NekkoYato <89446086+NekkoYato@users.noreply.github.com>
2021-12-20 11:47:06 +0530

    I'm dumb

--- Yukki/Plugins/Ping.py
+++ Yukki/Plugins/Ping.py
@@ -1,20 +1,33 @@
 import os
 import time
 from datetime import datetime
-
 import psutil
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
 from Yukki import BOT_USERNAME, MUSIC_BOT_NAME, app, boottime
 from Yukki.Utilities.ping import get_readable_time
-
 __MODULE__ = "Ping"
 __HELP__ = """
-
 /ping - Check if Bot is alive or not.
 """
 
+# ===============================================
+normaltext = "1234567890."
+pingfont = [
+    "𝟭",
+    "𝟮",
+    "𝟯",
+    "𝟰",
+    "𝟱",
+    "𝟲",
+    "𝟳",
+    "𝟴",
+    "𝟵",
+    "𝟬",
+    "•",
+]
+
+# ===============================================
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - boottime)
@@ -27,8 +40,6 @@ CPU: {cpu}%
 RAM: {mem}%
 Disk: {disk}%"""
     return stats
-
-
 @app.on_message(filters.command(["ping", f"ping@{BOT_USERNAME}"]))
 async def ping(_, message):
     start = datetime.now()
@@ -39,6 +50,11 @@ async def ping(_, message):
     uptime = await bot_sys_stats()
     end = datetime.now()
     resp = (end - start).microseconds / 1000
+    resp = str((end - start).microseconds / 1000)
+    for normal in resp:
+        if normal in normaltext:
+            pingchars = pingfont[normaltext.index(normal)]
+            resp = resp.replace(normal, pingchars)    
     await response.edit_text(
         f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} System Stats:</u></b>{uptime}"
     )
