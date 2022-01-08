f603635e9dae147ba3ba5982eae0ed54157ca97e
Starzbots <96718781+Starzbots@users.noreply.github.com>
2022-01-08 14:29:49 +0530

    Update Yukki.py

--- Yukki/Core/PyTgCalls/Yukki.py
+++ Yukki/Core/PyTgCalls/Yukki.py
@@ -167,7 +167,7 @@ async def stream_end_handler(_, update: Update):
                 "28492",
                 aud,
             )
-    except Exception as ee:
+    except Exception as e:
         print(e)
 
 
